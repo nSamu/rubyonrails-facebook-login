c61cd11e8ba1a7d171e6d9eb8bfd242b77354430
Nagy Samu <h059417@u-szeged.hu>
2014-10-26 22:54:50 +0100

    "Clean" rails 3.2.19 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookLogin::Application.config.secret_token = '687cda6ee8fd144f23e841d9beeceb66d0c64aed9620829f6bf7c54d1266af507eff9a2aafce5f7c175942e43496c44eabcf6449719d0da814317f16d128c3d9'
